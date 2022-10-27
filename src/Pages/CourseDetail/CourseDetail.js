d0ae5d8fc30b99dab1970d1821918f7ba352ef48
Tamannarahman <tamannarahmanmridha@gmail.com>
2022-10-27 22:10:42 +0600

    courses details added

--- src/Pages/CourseDetail/CourseDetail.js
+++ src/Pages/CourseDetail/CourseDetail.js
@@ -7,15 +7,18 @@ const CourseDetail = () => {
     const { name, img, description, fee, duration, eligibility } = selectedCourse
 
     return (
-        <div className='container'>
+        <div className='container '>
 
-            <div className="card card-compact  bg-base-100 shadow-xl">
-                <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                <figure><img src={img} alt="Shoes" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">{name}</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <h1 className="card-title font-bold">{name}</h1>
+                    <p>{description}</p>
+                    <h3>Fee: {fee}</h3>
+                    <h6>Course Duration: {duration}</h6>
+                    <h6>Eligibility: {eligibility}</h6>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                        <button className="btn btn-success">Get Premium Access</button>
                     </div>
                 </div>
             </div>
